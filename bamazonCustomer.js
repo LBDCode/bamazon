eb6fe71e1e76a4c6e46ac8727ec6ce72ac401cc1
LBDCode <libby.duggan@gmail.com>
2019-01-18 13:51:14 -0500

    basic customer functionality

---
+++ bamazonCustomer.js
@@ -0,0 +1,100 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var cTable = require('console.table');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log ("connected: " + connection.threadId);
+    
+    displayInventory();
+});
+
+function displayInventory() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        customerSale();
+    });
+};
+
+function customerSale() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        inquirer
+        .prompt([
+            {
+                name:"product",
+                type:"rawlist",
+                choices: function() {
+                    var invArray = [];
+                    for (var i = 0; i < res.length; i++) {
+                        invArray.push(res[i].product_name);
+                    }
+                    return invArray;
+                },
+                message: "What item would you like to purchase?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ])
+        .then(function(ans) {
+
+            if (parseInt(ans.quantity) <= 0) {
+                console.log("\nPlease select an item quantity greater than zero.\n")
+                displayInventory();
+            } else {
+                var purchItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === ans.product) {
+                        purchItem = res[i];
+                    }
+                }
+
+                if (purchItem.stock_quantity < parseInt(ans.quantity)) {
+                    console.log("\nSorry!  There aren't enough items on hand to complete your purchase.  \nPlease select a different item or quantity.\n")
+                    displayInventory();
+                } else {
+                    var purchTotal = parseInt(ans.quantity) * purchItem.price;
+                    var new_quant = purchItem.stock_quantity - parseInt(ans.quantity);
+                    
+                    console.log("\nThank you for your purchase!\nYou bought " 
+                    + ans.quantity + " " + ans.product + "(s) for a total of $" 
+                    + purchTotal + ".\n");
+                    updateDB(purchItem.item_id, new_quant);
+                }
+            }
+        
+        });
+    });
+};
+
+function updateDB(item, new_item_quant) {
+    //update the DB and display new inventory
+    connection.query("UPDATE products SET ? WHERE ?",
+        [
+            {
+            stock_quantity: new_item_quant
+            },
+            {
+            item_id: item
+            }
+        ],
+        function(err) {
+            if (err) throw err;
+            displayInventory();
+        }
+    );
+};
---
+++ seeds.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+
+CREATE TABLE products (
+	item_id INT NOT NULL auto_increment,
+    product_name VARCHAR(100),
+    department_name VARCHAR(100),
+    price DECIMAL (10,2),
+    stock_quantity INT,
+    primary key (item_id)
+);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Squeeky Duck Toy", "pets", 7.50, 10), 
+("Headphones", "electronics", 8.75, 35),
+("Multivitamin", "health", 14.50, 200),
+("Crest Toothpaste", "health", 3.15, 94),
+("Sanuk Flip-Flops", "shoes", 19.44, 8),
+("Coppertone Sunscreen", "health", 6.50, 250),
+("Saucony Pro-grid 9", "shoes", 68.90, 52),
+("Diamond Naturals Dog Food", "pets", 38.60, 31),
+("LG OLED HD Smart TV", "electronics", 2230.87, 7);
+
